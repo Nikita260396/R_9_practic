2c9975235e5f655b8899a666d358472dd416af1c
Никита <71729237+Nikita260396@users.noreply.github.com>
2020-11-13 00:43:57 +0300

    Update histogramm_statistic.R

--- histogramm_statistic.R
+++ histogramm_statistic.R
@@ -1,1 +1,17 @@
-
+histogramm_statistic<-function(n,mean_x,sd_x,mean_e,sd_e)
+{
+set.seed(6)
+x< -rnorm(n,mean_x,sd_x)##параметры x
+e< -rnorm(n,mean_e,sd_e)##параметры e
+y<-100-12*x+e
+plot (x,y)
+attach(mtcars)
+layout(matrix(2:1,1, 2, byrow = T))
+hist(x, col="peachpuff", border="black", prob = TRUE, xlab="temp", main = "Histogramm #2")
+lines(density(x), col="red", lwd =2)
+curve(dnorm(x, mean = mean(x), sd = sd(x)), col = "darkblue", lwd = 2, add = T)
+hist(y, col="peachpuff", border="black", prob = TRUE, xlab="temp", main = "Histogramm #1")
+lines(density(y), col="red", lwd =2)
+curve(dnorm(y, mean = mean(y), sd = sd(y)), col = "darkblue", lwd = 2, add = T, xname = "y")
+detach(mtcars)
+}
